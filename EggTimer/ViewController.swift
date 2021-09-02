1114cb8ded83a532bf48e3e21f569ff94d41f189
Marymezh <maria.mezhova@yahoo.com>
2021-09-02 11:20:14 +0700

    prints for different egg types

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+//   3 constans method
+//    let softTime = 5
+//    let mediumTime = 7
+//    let hardTime = 12
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
 
-
+    @IBAction func EggTypeSelected(_ sender: UIButton) {
+        
+        guard let hardness = sender.currentTitle else { return }
+        
+        guard let result = eggTimes[hardness] else { return }
+        
+        
+        print (result)
+        
+//  swith-case method
+//        switch hardness {
+//
+//        case "Soft": print ("\(softTime) minutes")
+//        case "Medium": print ("\(mediumTime) minutes")
+//        case "Hard": print ("\(hardTime) minutes")
+//        default: break
+//        }
+        
+// if-else method
+//        if hardness == "Soft" {
+//            print ("\(softTime) minutes")
+//        } else if hardness == "Medium" {
+//            print ("\(mediumTime) minutes")
+//        } else {
+//            print ("\(hardTime) minutes")
+//        }
+        
+    }
+    
+    
 }
